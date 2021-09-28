8ee6950b8f0040baf2a62fd0dbb28169fb484197
unknown <miemail@dominio.com>
2021-09-28 18:31:14 -0500

    onkeyup_onblur

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function myFuncion(){ //funcion que convierte las letras a mayusculas
+    var x= document.getElementById("fname"); //declaramos una variable y cargamos el input en ella
+    x.value = x.value.toUpperCase(); // toUpperCase()= convertir en mayusculas  toLowerCase()
+
+    //slice()
+
+}
+
+function myFuncion2(){
+    var y= document.getElementById("fapellido");//declaramos una variable y cargamos el input en ella
+    y.value = y.value.toUpperCase();
+}
